bf032156f098440c5648ec54550fa47d8c7abc67
wangbin <853500842@qq.com>
2019-07-12 18:58:22 +0800

    翻译

--- connect/api/src/main/java/org/apache/kafka/connect/data/Timestamp.java
+++ connect/api/src/main/java/org/apache/kafka/connect/data/Timestamp.java
@@ -20,16 +20,23 @@ import org.apache.kafka.connect.errors.DataException;
 
 /**
  * <p>
+ *     corresponding 相应的  underlying根本的，底层的
  *     A timestamp representing an absolute time, without timezone information. The corresponding Java type is a
+ *     表示绝对时间的时间戳，没有时区信息，相应的java类型是Date，底层表示是一个长的表示自Unix epoch以来的毫秒数
  *     java.util.Date. The underlying representation is a long representing the number of milliseconds since Unix epoch.
  * </p>
  */
 public class Timestamp {
+    /**
+     * 如果拉开了时间帷幕来窥视一眼，我们职业生涯在21世纪是很漫长的，起码30年是有的，所以眼下的未知和迷茫是可以拨开的
+     */
     public static final String LOGICAL_NAME = "org.apache.kafka.connect.data.Timestamp";
 
     /**
      * Returns a SchemaBuilder for a Timestamp. By returning a SchemaBuilder you can override additional schema settings such
      * as required/optional, default value, and documentation.
+     * 返回时间戳的SchemaBuilder，通过返回SchemaBuilder，您可以覆盖其他模式设置，如必需/可选，默认值和文档
+     *
      * @return a SchemaBuilder
      */
     public static SchemaBuilder builder() {
@@ -42,8 +49,9 @@ public class Timestamp {
 
     /**
      * Convert a value from its logical format (Date) to it's encoded format.
-     * @param value the logical value
-     * @return the encoded value
+     * 将值从其逻辑格式（日期）转换为其编码格式
+     * @param value the logical value 逻辑值
+     * @return the encoded value 编码值
      */
     public static long fromLogical(Schema schema, java.util.Date value) {
         if (!(LOGICAL_NAME.equals(schema.name())))
