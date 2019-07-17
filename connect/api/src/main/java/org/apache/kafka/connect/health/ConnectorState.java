3c39a6e546f499e2d1f3f263eca9584f7d5f627d
wangbin <853500842@qq.com>
2019-07-17 11:21:17 +0800

    连接器的状态和标识符判断有冲突，不知道作者知否还有其他的用意，反正没看明白

--- connect/api/src/main/java/org/apache/kafka/connect/health/ConnectorState.java
+++ connect/api/src/main/java/org/apache/kafka/connect/health/ConnectorState.java
@@ -19,13 +19,14 @@ package org.apache.kafka.connect.health;
 
 /**
  * Describes the status, worker ID, and any errors associated with a connector.
+ *  描述与连接器相关的状态，工作id，错误信息
  */
 public class ConnectorState extends AbstractState {
 
     /**
      * Provides an instance of the ConnectorState.
-     *
-     * @param state - the status of connector, may not be {@code null} or empty
+     * 提供一个连接器状态实例
+     * @param state - the status of connector, may not be {@code null} or empty  连接器的状态不能为@code null或空
      * @param workerId - the workerId associated with the connector, may not be {@code null} or empty
      * @param traceMessage - any error message associated with the connector, may be {@code null} or empty
      */
