f29303cce0e9e5ea178fd2fb875721bc4fd21231
wangbin <853500842@qq.com>
2019-07-22 18:56:46 +0800

    从其他系统导入kafka或者从kafka导出数据到其他系统的顶级接口

--- connect/api/src/main/java/org/apache/kafka/connect/connector/Task.java
+++ connect/api/src/main/java/org/apache/kafka/connect/connector/Task.java
@@ -20,12 +20,16 @@ import java.util.Map;
 
 /**
  * <p>
- * Tasks contain the code that actually copies data to/from another system. They receive
- * a configuration from their parent Connector, assigning them a fraction of a Kafka Connect job's work.
- * The Kafka Connect framework then pushes/pulls data from the Task. The Task must also be able to
- * respond to reconfiguration requests.
+ *     copies 副本，复印件
+ * Tasks contain the code that actually copies data to/from another system.
+ *  任务包含将数据复制到其他系统或从其他系统复制数据的代码
+ * They receive a configuration from their parent Connector, assigning them a fraction of a Kafka Connect job's work.
+ * 它们从其父连接器接收配置，为它们分配Kafka Connect作业的一部分工作
+ * The Kafka Connect framework then pushes/pulls data from the Task. The Task must also be able to respond to reconfiguration requests.
+ *  kafka连接框架从任务中推送或拉取数据。此任务必须能够响应重新配置请求
  * </p>
  * <p>
+ *     minimal 极小的，最小的   shared共有，共享   functionality实用，功能
  * Task only contains the minimal shared functionality between
  * {@link org.apache.kafka.connect.source.SourceTask} and
  * {@link org.apache.kafka.connect.sink.SinkTask}.
@@ -33,8 +37,9 @@ import java.util.Map;
  */
 public interface Task {
     /**
+     * corresponding 符合的，相应的，类似于
      * Get the version of this task. Usually this should be the same as the corresponding {@link Connector} class's version.
-     *
+     *  获取任务版本号。通常这应该与对应的Connector类的版本相同
      * @return the version, formatted as a String
      */
     String version();
